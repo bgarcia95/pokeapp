9993f6008210f31d651692436a023c025abc7db2
Brayan Garcia <bgarcia.95@outlook.com>
2020-09-20 15:33:43 -0600

    Refactor login screen.

--- src/screens/Auth/Login.js
+++ src/screens/Auth/Login.js
@@ -1,47 +1,28 @@
 // import 'react-native-gesture-handler';
-import React, {useState, useEffect} from 'react';
-import {
-  SafeAreaView,
-  View,
-  Text,
-  StatusBar,
-  Button,
-  Image,
-  ActivityIndicator,
-  StyleSheet,
-} from 'react-native';
-
-import {Header, Colors} from 'react-native/Libraries/NewAppScreen';
+import React, {useEffect} from 'react';
+import {View, StyleSheet, Text, Image} from 'react-native';
 import {
   GoogleSignin,
   GoogleSigninButton,
   statusCodes,
 } from '@react-native-community/google-signin';
 import auth from '@react-native-firebase/auth';
-import database from '@react-native-firebase/database';
+import {useDispatch} from 'react-redux';
+import {authenticate, logout} from '../../redux/actions/auth';
 
 const Login = (props) => {
-  const [loggedIn, setloggedIn] = useState(false);
-  const [user, setUser] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [showHome, setShowHome] = useState(false);
+  const dispatch = useDispatch();
 
   const _signIn = async () => {
     try {
       await GoogleSignin.hasPlayServices();
       const {accessToken, idToken} = await GoogleSignin.signIn();
 
-      setLoading(true);
-      setloggedIn(true);
-
       const credential = auth.GoogleAuthProvider.credential(
         idToken,
         accessToken,
       );
       await auth().signInWithCredential(credential);
-
-      setLoading(false);
-      props.navigation.replace('Home');
     } catch (error) {
       if (error.code === statusCodes.SIGN_IN_CANCELLED) {
         // user cancelled the login flow
@@ -57,26 +38,6 @@ const Login = (props) => {
       }
     }
   };
-  const onAuthStateChanged = (user) => {
-    console.log('User!!', user);
-    setUser(user);
-    console.log(user);
-    if (user) {
-      setloggedIn(true);
-    } else {
-      setloggedIn(false);
-    }
-  };
-  useEffect(() => {
-    GoogleSignin.configure({
-      scopes: ['email'], // what API you want to access on behalf of the user, default is email and profile
-      webClientId:
-        '140571925073-nnfpp72amof458ljsk82l5hsaohq3mlo.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
-      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
-    });
-    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber; // unsubscribe on unmount
-  }, []);
 
   // const newReference = database().ref('/usuarios');
   // const handleSubmit = () => {
@@ -88,91 +49,52 @@ const Login = (props) => {
   //     .then(() => console.log('Data updated.'));
   // };
 
-  const signOut = async () => {
-    try {
-      setLoading(true);
-      await GoogleSignin.revokeAccess();
-      await GoogleSignin.signOut();
-      auth()
-        .signOut()
-        .then(() => setLoading(false));
-      setloggedIn(false);
-      setUser([]);
-    } catch (error) {
-      console.error(error);
+  const onAuthStateChanged = (user) => {
+    if (user) {
+      console.log('true');
+      dispatch(authenticate(user));
+    } else {
+      console.log('false');
+      dispatch(logout());
     }
   };
 
-  if (loading) {
-    return (
-      <View style={styles.screen}>
-        <ActivityIndicator color="#353535" size="large" />
-      </View>
-    );
-  }
+  useEffect(() => {
+    GoogleSignin.configure({
+      scopes: ['email'],
+      webClientId:
+        '140571925073-nnfpp72amof458ljsk82l5hsaohq3mlo.apps.googleusercontent.com',
+      offlineAccess: true,
+    });
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
   return (
     <View style={styles.screen}>
-      {!loggedIn && (
+      <View style={{flexDirection: 'row', alignItems: 'center'}}>
+        <Text style={styles.title}>PokeApp</Text>
         <View
           style={{
-            flexDirection: 'row',
-            alignItems: 'center',
+            height: 30,
+            width: 30,
+            marginLeft: 5,
+            marginBottom: 10,
           }}>
-          <Text style={styles.title}>PokeApp</Text>
-          <View
-            style={{
-              height: 30,
-              width: 30,
-              marginLeft: 5,
-              marginBottom: 10,
-            }}>
-            <Image
-              source={{
-                uri:
-                  'https://res.cloudinary.com/bgarcia95/image/upload/v1600496542/pokeapp/pokeball_bdt5vr.png',
-              }}
-              style={{height: '100%', width: '100%', marginBottom: 20}}
-            />
-          </View>
-        </View>
-      )}
-      <View>
-        {!loggedIn && (
-          <GoogleSigninButton
-            style={{width: 192, height: 48}}
-            size={GoogleSigninButton.Size.Wide}
-            color={GoogleSigninButton.Color.Dark}
-            onPress={_signIn}
+          <Image
+            source={{
+              uri:
+                'https://res.cloudinary.com/bgarcia95/image/upload/v1600496542/pokeapp/pokeball_bdt5vr.png',
+            }}
+            style={{height: '100%', width: '100%', marginBottom: 20}}
           />
-        )}
+        </View>
       </View>
-
-      <View style={styles.buttonContainer}>
-        {/* {!user && <Text>You are currently logged out</Text>} */}
-        {user && (
-          <View>
-            <Text>Welcome {user.displayName}</Text>
-            <View
-              style={{
-                height: 150,
-                width: 150,
-                borderRadius: 75,
-                overflow: 'hidden',
-              }}>
-              <Image
-                source={{uri: user.photoURL}}
-                style={{height: '100%', width: '100%'}}
-              />
-            </View>
-            <Button
-              onPress={() => setShowHome(true)}
-              title="Go Home"
-              color="purple"
-            />
-            <Button onPress={signOut} title="LogOut" color="red" />
-          </View>
-        )}
-      </View>
+      <GoogleSigninButton
+        style={{width: 192, height: 48}}
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={_signIn}
+      />
     </View>
   );
 };
@@ -186,29 +108,6 @@ const styles = StyleSheet.create({
 
   title: {
     fontFamily: 'PressStart2P-Regular',
-  },
-
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
   },
 });
 
