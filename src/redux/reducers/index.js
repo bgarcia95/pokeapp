fbf0dd0fd4966cf28429c7abc70e3264e70051f4
Brayan Garcia <bgarcia.95@outlook.com>
2020-09-20 15:29:08 -0600

    Add regions reducer to redux store

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import authReducer from './auth';
+import regionsReducer from './regions';
 import pokemonsReducer from './pokemons';
 
 export default combineReducers({
   auth: authReducer,
+  regions: regionsReducer,
   pokemons: pokemonsReducer,
 });
