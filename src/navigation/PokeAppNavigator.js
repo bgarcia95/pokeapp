787e7f8b1d263d94d4d9c324fa6f6c4df2bbc582
Brayan Garcia <bgarcia.95@outlook.com>
2020-09-21 06:17:54 -0600

    Add Teams own navigator and add it to existing bottom tab navigator

--- src/navigation/PokeAppNavigator.js
+++ src/navigation/PokeAppNavigator.js
@@ -4,6 +4,23 @@ import Teams from '../screens/Teams';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import {createStackNavigator} from '@react-navigation/stack';
 import Profile from '../screens/Profile';
+import TeamsManagement from '../screens/TeamsManagement';
+import ViewTeam from '../screens/ViewTeam';
+
+const TeamsStackNavigator = createStackNavigator();
+
+export const TeamsNavigator = () => {
+  return (
+    <TeamsStackNavigator.Navigator headerMode="none">
+      <TeamsStackNavigator.Screen name="TeamsScreen" component={Teams} />
+      <TeamsStackNavigator.Screen
+        name="TeamsManagementScreen"
+        component={TeamsManagement}
+      />
+      <TeamsStackNavigator.Screen name="ViewTeam" component={ViewTeam} />
+    </TeamsStackNavigator.Navigator>
+  );
+};
 
 const MainStackNavigator = createMaterialBottomTabNavigator();
 
@@ -14,7 +31,7 @@ export const MainNavigator = () => {
       shifting={true}
       backBehavior="history">
       <MainStackNavigator.Screen name="Profile" component={Profile} />
-      <MainStackNavigator.Screen name="Teams" component={Teams} />
+      <MainStackNavigator.Screen name="Teams" component={TeamsNavigator} />
     </MainStackNavigator.Navigator>
   );
 };
