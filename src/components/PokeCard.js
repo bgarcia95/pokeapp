f05184ed3ab2c1ef335b5086d1cfc27a0aae129a
Brayan Garcia <bgarcia.95@outlook.com>
2020-09-21 20:31:02 -0600

    Remove inline styles and add them into stylesheet properties. Add pokemon's 'type' information.

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
-import {ActivityIndicator, Image, Text, View} from 'react-native';
+import {ActivityIndicator, Image, StyleSheet, Text, View} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
-import {poly} from 'react-native/Libraries/Animated/src/Easing';
 
 const PokeCard = ({pokemon, selectedValues, setSelectedValues, isViewing}) => {
   const foundPokemon = selectedValues?.find((pok) => pok.id === pokemon.id);
@@ -26,39 +25,24 @@ const PokeCard = ({pokemon, selectedValues, setSelectedValues, isViewing}) => {
 
   return (
     <View
-      style={{
-        flex: 1,
-        margin: 10,
-        borderRadius: 10,
-        backgroundColor: !selectedPokemon ? '#fff' : 'rgba(0,0,0,0.2)',
-        elevation: !selectedPokemon ? 5 : 0,
-      }}>
+      style={[
+        styles.cardItem,
+        {
+          backgroundColor: !selectedPokemon ? '#fff' : 'rgba(0,0,0,0.2)',
+          elevation: !selectedPokemon ? 5 : 0,
+        },
+      ]}>
       {!isViewing ? (
         <TouchableOpacity onPress={onPressedCard} activeOpacity={0.6}>
           <View>
-            <View
-              style={{
-                flex: 1,
-                borderRadius: 10,
-                padding: 15,
-              }}>
-              <View
-                style={{
-                  width: 150,
-                  elevation: 5,
-                }}>
-                <View
-                  style={{
-                    height: 100,
-                    width: '100%',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                  }}>
+            <View style={styles.cardContainer}>
+              <View style={styles.cardContent}>
+                <View style={styles.imageContainer}>
                   {!pokemon.image && pokemon.image !== null ? (
                     <ActivityIndicator color="red" size="small" />
                   ) : (
                     <Image
-                      style={{height: '100%', width: 100, alignSelf: 'center'}}
+                      style={styles.image}
                       source={
                         pokemon.image !== null
                           ? {
@@ -69,44 +53,40 @@ const PokeCard = ({pokemon, selectedValues, setSelectedValues, isViewing}) => {
                     />
                   )}
                 </View>
-                <Text># {pokemon.id}</Text>
-                <Text style={{fontWeight: 'bold', textTransform: 'capitalize'}}>
-                  {pokemon.name}
-                </Text>
+                <Text style={styles.pokemonId}># {pokemon.id}</Text>
+                <Text style={styles.pokemonName}>{pokemon.name}</Text>
                 <Text>{pokemon.description}</Text>
+                {pokemon.types && (
+                  <View style={styles.row}>
+                    <Text style={styles.type}>Type:</Text>
+                    {pokemon.types?.map((type, index) => (
+                      <Text key={index} style={styles.typeName}>
+                        {type.type.name}
+                        {pokemon.types?.length > 1 && index === 0 && (
+                          <Text> / </Text>
+                        )}
+                      </Text>
+                    ))}
+                  </View>
+                )}
               </View>
             </View>
           </View>
         </TouchableOpacity>
       ) : (
-        <View
-          style={{
-            flex: 1,
-            borderRadius: 10,
-            padding: 15,
-          }}>
-          <View
-            style={{
-              width: '100%',
-              elevation: 5,
-            }}>
-            <View
-              style={{
-                height: 100,
-                width: '100%',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
+        <View style={styles.cardContainer}>
+          <View style={styles.cardContent}>
+            <View style={styles.imageContainer}>
               {!pokemon.image ? (
                 <Image
-                  style={{height: 80, width: 80, alignSelf: 'center'}}
+                  style={styles.image}
                   source={require('../../assets/img/notavailable.png')}
                 />
               ) : !pokemon.image && pokemon.image !== null ? (
                 <ActivityIndicator color="red" size="small" />
               ) : (
                 <Image
-                  style={{height: '100%', width: 100, alignSelf: 'center'}}
+                  style={styles.image}
                   source={
                     pokemon.image !== null
                       ? {
@@ -117,16 +97,86 @@ const PokeCard = ({pokemon, selectedValues, setSelectedValues, isViewing}) => {
                 />
               )}
             </View>
-            <Text># {pokemon.id}</Text>
-            <Text style={{fontWeight: 'bold', textTransform: 'capitalize'}}>
-              {pokemon.name}
-            </Text>
-            <Text>{pokemon.description}</Text>
+            <Text style={styles.pokemonId}># {pokemon.id}</Text>
+            <Text style={styles.pokemonName}>{pokemon.name}</Text>
+            <Text style={styles.pokemonDescription}>{pokemon.description}</Text>
+            {pokemon.types && (
+              <View style={styles.row}>
+                <Text style={styles.type}>Type:</Text>
+                {pokemon.types?.map((type, index) => (
+                  <Text key={index} style={styles.typeName}>
+                    {type.type.name}
+                    {pokemon.types?.length > 1 && index === 0 && (
+                      <Text> / </Text>
+                    )}
+                  </Text>
+                ))}
+              </View>
+            )}
           </View>
         </View>
       )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  cardItem: {
+    flex: 1,
+    margin: 10,
+    borderRadius: 10,
+  },
+  cardContainer: {
+    flex: 1,
+    borderRadius: 10,
+    padding: 15,
+  },
+  cardContent: {
+    flex: 1,
+    elevation: 5,
+  },
+  imageContainer: {
+    height: 100,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {height: '100%', width: 100, alignSelf: 'center'},
+  pokemonId: {
+    fontFamily: 'OpenSans-Regular',
+    color: 'gray',
+    fontSize: 16,
+  },
+  pokemonName: {
+    fontWeight: 'bold',
+    textTransform: 'capitalize',
+    fontFamily: 'OpenSans-Regular',
+    fontSize: 24,
+  },
+  pokemonDescription: {
+    fontFamily: 'OpenSans-Regular',
+    fontSize: 16,
+    marginVertical: 5,
+  },
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+    marginVertical: 5,
+  },
+  type: {
+    fontFamily: 'OpenSans-Regular',
+    textTransform: 'capitalize',
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginRight: 5,
+  },
+  typeName: {
+    fontFamily: 'OpenSans-Regular',
+    textTransform: 'capitalize',
+    fontSize: 14,
+    color: '#353535',
+  },
+});
 
 export default PokeCard;
