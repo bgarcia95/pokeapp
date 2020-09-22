bbadea64211de6b4cd3ddb23835e0161d4f7c494
Brayan Garcia <bgarcia.95@outlook.com>
2020-09-21 20:25:21 -0600

    Add styles to bottom tabs and stack navigator. Updating Navigation logic.

--- src/navigation/PokeAppNavigator.js
+++ src/navigation/PokeAppNavigator.js
@@ -6,18 +6,36 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Profile from '../screens/Profile';
 import TeamsManagement from '../screens/TeamsManagement';
 import ViewTeam from '../screens/ViewTeam';
+import AntDesignIcon from 'react-native-vector-icons/AntDesign';
+import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {Text} from 'react-native';
+import Colors from '../constants/Colors';
+
+const defaultNavOptions = {
+  headerStyle: {
+    backgroundColor: Colors.primary,
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontFamily: 'OpenSans-Regular',
+  },
+};
 
 const TeamsStackNavigator = createStackNavigator();
 
-export const TeamsNavigator = () => {
+export const AppMainNavigator = () => {
   return (
-    <TeamsStackNavigator.Navigator headerMode="none">
-      <TeamsStackNavigator.Screen name="TeamsScreen" component={Teams} />
+    <TeamsStackNavigator.Navigator screenOptions={defaultNavOptions}>
+      <TeamsStackNavigator.Screen
+        name="Home"
+        component={MainNavigator}
+        options={{headerShown: false}}
+      />
+      <TeamsStackNavigator.Screen name="ViewTeam" component={ViewTeam} />
       <TeamsStackNavigator.Screen
         name="TeamsManagementScreen"
         component={TeamsManagement}
       />
-      <TeamsStackNavigator.Screen name="ViewTeam" component={ViewTeam} />
     </TeamsStackNavigator.Navigator>
   );
 };
@@ -30,8 +48,42 @@ export const MainNavigator = () => {
       activeColor="#fff"
       shifting={true}
       backBehavior="history">
-      <MainStackNavigator.Screen name="Profile" component={Profile} />
-      <MainStackNavigator.Screen name="Teams" component={TeamsNavigator} />
+      <MainStackNavigator.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: (tabInfo) => (
+            <AntDesignIcon name="user" size={24} color={tabInfo.color} />
+          ),
+          tabBarColor: Colors.primary,
+          tabBarLabel:
+            Platform.OS === 'android' ? (
+              <Text style={{fontFamily: 'OpenSans-Regular'}}>Profile</Text>
+            ) : (
+              'Profile'
+            ),
+        }}
+      />
+      <MainStackNavigator.Screen
+        name="TeamsScreen"
+        component={Teams}
+        options={{
+          tabBarIcon: (tabInfo) => (
+            <MaterialCommunityIcon
+              name="pokemon-go"
+              size={24}
+              color={tabInfo.color}
+            />
+          ),
+          tabBarColor: Colors.accent,
+          tabBarLabel:
+            Platform.OS === 'android' ? (
+              <Text style={{fontFamily: 'OpenSans-Regular'}}>Teams</Text>
+            ) : (
+              'Teams'
+            ),
+        }}
+      />
     </MainStackNavigator.Navigator>
   );
 };
@@ -40,7 +92,7 @@ const AuthStackNavigator = createStackNavigator();
 
 export const AuthNavigator = () => {
   return (
-    <AuthStackNavigator.Navigator>
+    <AuthStackNavigator.Navigator headerMode="none">
       <AuthStackNavigator.Screen name="Login" component={Login} />
     </AuthStackNavigator.Navigator>
   );
