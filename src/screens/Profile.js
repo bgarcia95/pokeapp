0a697e0b4fc891b4eba7a649f4605388bab0392b
Brayan Garcia <bgarcia.95@outlook.com>
2020-09-21 20:24:34 -0600

    Remove unused imports. Add image on top left of the screen. Remove inline styles and add them into stylesheet properties

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,25 +1,11 @@
-import React, {useState, useEffect} from 'react';
-import {
-  SafeAreaView,
-  View,
-  Text,
-  StatusBar,
-  Button,
-  Image,
-  ActivityIndicator,
-  StyleSheet,
-} from 'react-native';
+import React, {useEffect} from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
 
-import {Header, Colors} from 'react-native/Libraries/NewAppScreen';
-import {
-  GoogleSignin,
-  GoogleSigninButton,
-  statusCodes,
-} from '@react-native-community/google-signin';
+import {GoogleSignin} from '@react-native-community/google-signin';
 import auth from '@react-native-firebase/auth';
-import database from '@react-native-firebase/database';
 import {useDispatch, useSelector} from 'react-redux';
 import {authenticate, logout} from '../redux/actions/auth';
+import {Button} from 'react-native-paper';
 
 const Profile = (props) => {
   const onAuthStateChanged = (user) => {
@@ -32,13 +18,13 @@ const Profile = (props) => {
 
   useEffect(() => {
     GoogleSignin.configure({
-      scopes: ['email'], // what API you want to access on behalf of the user, default is email and profile
+      scopes: ['email'],
       webClientId:
-        '140571925073-nnfpp72amof458ljsk82l5hsaohq3mlo.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
-      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+        '140571925073-nnfpp72amof458ljsk82l5hsaohq3mlo.apps.googleusercontent.com',
+      offlineAccess: true,
     });
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber; // unsubscribe on unmount
+    return subscriber;
   }, []);
 
   const signOut = async () => {
@@ -59,25 +45,34 @@ const Profile = (props) => {
 
   return (
     <View style={styles.screen}>
-      <View style={styles.buttonContainer}>
-        <View style={{marginVertical: 10}}>
-          <Text>Welcome {user.displayName}</Text>
+      <View style={styles.backgroundImageContainer}>
+        <Image
+          source={require('../../assets/img/poke-bg.png')}
+          style={styles.backgroundImage}
+        />
+      </View>
+      <View style={styles.screen}>
+        <View style={styles.welcomeContainer}>
+          <Text style={styles.welcomeText}>
+            Welcome <Text style={styles.userName}>{user.displayName} !</Text>
+          </Text>
         </View>
-        <View
-          style={{
-            height: 150,
-            width: 150,
-            borderRadius: 75,
-            overflow: 'hidden',
-            marginVertical: 10,
-          }}>
+        <View style={styles.userPictureWrapper}>
           <Image
             source={{uri: user.photoURL}}
             style={{height: '100%', width: '100%'}}
           />
         </View>
 
-        <Button onPress={signOut} title="LogOut" color="red" />
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={signOut}
+            color="#ED1B24"
+            mode="contained"
+            contentStyle={{width: '100%', height: '100%'}}>
+            Logout
+          </Button>
+        </View>
       </View>
     </View>
   );
@@ -89,5 +84,38 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+
+  backgroundImageContainer: {
+    position: 'absolute',
+    top: -80,
+    alignSelf: 'flex-end',
+    left: 255,
+  },
+  backgroundImage: {
+    width: 250,
+    height: 250,
+  },
+  welcomeContainer: {
+    marginVertical: 10,
+  },
+  welcomeText: {
+    fontSize: 24,
+    fontFamily: 'OpenSans-Regular',
+  },
+  userName: {
+    fontSize: 24,
+    color: 'gray',
+  },
+  userPictureWrapper: {
+    height: 150,
+    width: 150,
+    borderRadius: 75,
+    overflow: 'hidden',
+    marginVertical: 10,
+    alignSelf: 'center',
+    borderColor: 'gray',
+    borderWidth: 5,
+  },
+  buttonContainer: {width: '100%', height: 50},
 });
 export default Profile;
