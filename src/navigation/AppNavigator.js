8f618d03fd98bf58d0255b1ff11f873b54dfcc44
Brayan Garcia <bgarcia.95@outlook.com>
2020-09-21 20:25:45 -0600

    Fix import, use updated navigator

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {useSelector} from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
 
-import {AuthNavigator, MainNavigator} from './PokeAppNavigator';
+import {AuthNavigator, AppMainNavigator} from './PokeAppNavigator';
 
 const AppNavigator = () => {
   const isAuth = useSelector((state) => !!state.auth.userData);
 
   return (
     <NavigationContainer>
-      {isAuth && <MainNavigator />}
+      {isAuth && <AppMainNavigator />}
       {!isAuth && <AuthNavigator />}
     </NavigationContainer>
   );
